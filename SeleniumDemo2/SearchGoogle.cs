83baa97b531db3f39bc0b9c0c449ab3775712c25
john johnson <johnson1324@comcast.net>
2020-01-15 16:22:20 -0800

    Another test3

--- SeleniumDemo2/SearchGoogle.cs
+++ SeleniumDemo2/SearchGoogle.cs
@@ -23,7 +23,7 @@ namespace SeleniumDemo2
         public void SearchForCheese()
         {
             var chromeOptions = new ChromeOptions();
-         chromeOptions.AddArguments("--headless");  // Need this  - comment it out to turn headless off.
+            //chromeOptions.AddArguments("--headless");  // Need this  - comment it out to turn headless off.
             // https://sqa.stackexchange.com/questions/33778/chromedriver-in-headless-mode-doesnt-work-correctly-because-of-windows-user-pol 
             // chromeOptions.AddArguments("--window-size=1920,1080");
             chromeOptions.AddArguments("--disable-gpu"); // Need this
